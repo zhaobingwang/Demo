8ca1ce8faeed44ddd7740f5850eb4139ff2e6e30
No.8 <zhaobingwang@foxmail.com>
2018-02-01 22:49:00 +0800

    async,await demo

--- AysncDemo/Program.cs
+++ AysncDemo/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace AysncDemo
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync().GetAwaiter().GetResult();   //模拟异步main
+        }
+
+        /// <summary>
+        /// 模拟异步main
+        /// </summary>
+        /// <returns></returns>
+        static async Task MainAsync()
+        {
+            int contentLength = await AccessTheWebAsync();
+            Console.WriteLine($"Length of downloaded string:{contentLength}");
+        }
+
+
+        static async Task<int> AccessTheWebAsync()
+        {
+            HttpClient client = new HttpClient();
+            Task<string> getStringTask = client.GetStringAsync("http://msdn.microsoft.com");
+            DoIndependentWork();
+            string urlContents = await getStringTask;
+            return urlContents.Length;
+        }
+        static void DoIndependentWork()
+        {
+            System.Console.WriteLine("Working......\r\n");
         }
     }
 }
