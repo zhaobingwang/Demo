29c26c4cb195375bce259e08cd1d45da2fe8092f
No.8 <zhaobingwang@foxmail.com>
2018-02-23 21:49:43 +0800

    +

---
+++ DotNetCoreConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetCoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
