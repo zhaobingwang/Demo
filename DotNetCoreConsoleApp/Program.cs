5db9d5cc57b9a2ddb32c1b385e44c74c4237384c
No.8 <zhaobingwang@foxmail.com>
2018-02-26 09:11:17 +0800

    配置管理-命令行配置，json配置

--- DotNetCoreConsoleApp/Program.cs
+++ DotNetCoreConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 
 namespace DotNetCoreConsoleApp
 {
@@ -6,7 +8,29 @@ namespace DotNetCoreConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("命令行配置：");
+            var settings = new Dictionary<string, string>
+            {
+                { "name","no8"},
+                { "age","20"}
+            };
+            var builder = new ConfigurationBuilder()
+                .AddInMemoryCollection(settings)
+                .AddCommandLine(args);
+            var configuration = builder.Build();
+            System.Console.WriteLine($"name:{configuration["name"]}");
+            System.Console.WriteLine($"name:{configuration["age"]}");
+
+            System.Console.WriteLine("Json配置：");
+            var builderJson = new ConfigurationBuilder()
+                .AddJsonFile("class.json");
+            var configurationJson = builderJson.Build();
+            System.Console.WriteLine($"ClassNo:{configurationJson["ClassNo"]}");
+            System.Console.WriteLine($"ClassNo:{configurationJson["ClassDesc"]}");
+            System.Console.WriteLine("Studdents:");
+            System.Console.WriteLine($"{configurationJson["Students:0:name"]}-{configurationJson["Students:0:age"]}");
+            System.Console.WriteLine($"{configurationJson["Students:1:name"]}-{configurationJson["Students:1:age"]}");
+            System.Console.WriteLine($"{configurationJson["Students:2:name"]}-{configurationJson["Students:2:age"]}");
         }
     }
 }
