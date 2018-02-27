ed0fc9b8d6dae4b0802f0c483feafc7bf44721e2
No.8 <zhaobingwang@foxmail.com>
2018-02-27 09:19:52 +0800

    值类型与引用类型

---
+++ Temp/ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v1 = 0;
+            int v2 = v1;
+            v2 = 1;
+            Console.WriteLine($"v1:{v1}  |  v2:{v2}");
+
+            User u1 = new User();
+            u1.Name = "张三";
+            u1.Age = 20;
+
+            User u2 = u1;
+            u2.Name = "李四";
+            u2.Age = 30;
+            Console.WriteLine($"u1.name:{u1.Name} | u1.age:{u1.Age}");
+        }
+    }
+
+    class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
