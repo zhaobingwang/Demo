6939faea86cf5b98b4e210cd7d044b7a77aa9097
赵炳旺 <zhaobingwang@sodao.com>
2018-03-27 18:58:33 +0800

    ActiveMQ

---
+++ ActiveMQ/Consumer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IConnectionFactory factory = new ConnectionFactory("tcp://localhost:61616");
+                using (IConnection connection = factory.CreateConnection())
+                {
+
+
+
+                    connection.ClientId = "firstQueueListener";
+                    connection.Start();
+                    using (ISession session = connection.CreateSession())
+                    {
+                        IMessageConsumer consumer = session.CreateConsumer(new Apache.NMS.ActiveMQ.Commands.ActiveMQQueue("firstQueue"), "testKey='testValue'");
+                        consumer.Listener += new MessageListener(consumer_listener);
+                        Console.Read();
+                    }
+                    connection.Stop();
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.Message);
+            }
+        }
+
+        private static void consumer_listener(IMessage message)
+        {
+            try
+            {
+                ITextMessage msg = (ITextMessage)message;
+                Console.WriteLine($"Receive:{msg.Text}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.Message);
+            }
+        }
+    }
+}
---
+++ ActiveMQ/Produce/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+
+namespace Produce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.创建ConnectoryFactory连接工厂
+            IConnectionFactory factory = new ConnectionFactory(("tcp://localhost:61616"));
+            
+            // 2.创建Connection连接
+            using (IConnection connection=factory.CreateConnection())
+            {
+                //connection.Start(); //默认关闭？
+                // 3.创建session？
+                using (ISession session=connection.CreateSession())
+                {
+                    // 4.创建Destination
+                    IDestination destination = new Apache.NMS.ActiveMQ.Commands.ActiveMQQueue("test");
+
+                    // 5.创建MessageProducer
+                    IMessageProducer producer = session.CreateProducer(destination);
+
+                    // 6.设置持久化方式
+                    producer.DeliveryMode = MsgDeliveryMode.NonPersistent;
+
+                    //7.定义消息对象，并发送
+                    ITextMessage message = session.CreateTextMessage();// producer.CreateTextMessage();
+                    message.Text = "test";
+                    message.Properties.SetString("testKey", "testValue");
+                    producer.Send(message);
+
+                    Console.WriteLine("send success");
+                    Console.Read();
+                }
+            }
+        }
+    }
+}
