0b694b31843c556ce7ff039d5cf5f4b89686db89
zhaobingwnag <zhaobingwang@sodao.com>
2018-05-16 20:11:12 +0800

    add tag entity

---
+++ UnitTest/Data/Entities/UserTag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Entities
+{
+    public class UserTag
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Tag { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
