cc2a784ee19e9b79000be2e5face037d3a06cf20
Route8 <zhaobingwang@foxmail.com>
2018-05-18 07:39:54 +0800

    temp

--- UnitTest/XUnitTestProject/UserServiceTest.cs
+++ UnitTest/XUnitTestProject/UserServiceTest.cs
@@ -1,6 +1,7 @@
 using Data;
 using Data.Entities;
 using Data.Service;
+using Moq;
 using System;
 using System.Threading.Tasks;
 using Xunit;
@@ -21,26 +22,40 @@ namespace XUnitTestProject
         [Fact(DisplayName = "新增一个用户")]
         public async Task UserService_Add_WithExpectedParameters()
         {
-            UserContext userContext = GetUerContext();
-            _userService = new UserService(userContext);
-            User entity = new User
+            //UserContext userContext = GetUerContext();
+            var mock = new Mock<UserContext>();
+
+            User user = new User
             {
                 NickName = "Jack",
                 CreateTime = DateTime.Now,
                 ModifyTime = DateTime.Now,
                 IsDelete = false
             };
-            User newUser = await _userService.AddUserAsync(entity);
-            _user = newUser;
-            Assert.True(newUser.Id > 0);
+
+            User expectedUser = new User
+            {
+                NickName = "Jack",
+                CreateTime = DateTime.Now,
+                ModifyTime = DateTime.Now,
+                IsDelete = false
+            };
+
+            //mock.Setup(c => c.Entry<User>(user)).Returns(;    //.ReturnsAsync(expectedUser);
+            _userService = new UserService(mock.Object);
+
+            User newUser = await _userService.AddUserAsync(user);
+            Assert.Equal(expectedUser, newUser);
+
+            //Assert.True(newUser.Id > 0);
         }
 
         public void Dispose()
         {
-            UserContext userContext = GetUerContext();
-            var entity = userContext.User.FindAsync(_user.Id).Result;
-            userContext.Remove(entity);
-            var success = userContext.SaveChangesAsync().Result > 0;
+            //UserContext userContext = GetUerContext();
+            //var entity = userContext.User.FindAsync(_user.Id).Result;
+            //userContext.Remove(entity);
+            //var success = userContext.SaveChangesAsync().Result > 0;
         }
     }
 }
