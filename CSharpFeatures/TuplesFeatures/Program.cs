83e44a2a2861ef858e4af1e9c159e8e62904e379
zhaobingwnag <zhaobingwang@sodao.com>
2018-05-19 15:27:20 +0800

    use tuples

--- CSharpFeatures/TuplesFeatures/Program.cs
+++ CSharpFeatures/TuplesFeatures/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TuplesFeatures
 {
@@ -6,7 +7,52 @@ namespace TuplesFeatures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // The first method of using tuples
+            List<int>list=new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            var result=Range(list);
+
+            Console.WriteLine("first:");
+            Console.WriteLine($"MAX:{result.Max};MIN:{result.Min}");
+            var result2=Range2(list);
+            Console.WriteLine("second:");
+            Console.WriteLine($"MAX:{result2.Item1};MIN:{result2.Item2}");
+        }
+        
+        /// <summary>
+        /// The first method of using tuples
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        private static (int Max,int Min) Range(IEnumerable<int> numbers)
+        {
+            int min=int.MaxValue;
+            int max=int.MinValue;
+            foreach (var item in numbers)
+            {
+                min = (item < min) ? item : min;
+                max = (item > max) ? item : max;
+            }
+            return (max,min);
+        }
+
+        /// <summary>
+        /// the second method of using tuples
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        private static (int,int) Range2(IEnumerable<int> numbers)
+        {
+            int min=int.MaxValue;
+            int max=int.MinValue;
+            foreach (var item in numbers)
+            {
+                min = (item < min) ? item : min;
+                max = (item > max) ? item : max;
+            }
+            return (max,min);
         }
     }
 }
