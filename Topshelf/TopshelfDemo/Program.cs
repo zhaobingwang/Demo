873c6250436a1d2b1f15ba15ec2944ed6bc593c0
zhaobingwnag <zhaobingwang@sodao.com>
2018-05-19 18:21:03 +0800

    TEMP

---
+++ Topshelf/TopshelfDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Topshelf;
+
+namespace TopshelfDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rc = HostFactory.Run(x =>                                   //1
+            {
+                x.Service<TownCrier>(s =>                                   //2
+                {
+                    s.ConstructUsing(name => new TownCrier());                //3
+                    s.WhenStarted(tc => tc.Start());                         //4
+                    s.WhenStopped(tc => tc.Stop());                          //5
+                });
+                x.RunAsLocalSystem();                                       //6
+
+                x.SetDescription("Sample Topshelf Host");                   //7
+                x.SetDisplayName("Stuff");                                  //8
+                x.SetServiceName("Stuff");                                  //9
+            });                                                             //10
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());  //11
+            Environment.ExitCode = exitCode;
+        }
+    }
+    public class TownCrier
+    {
+        readonly Timer _timer;
+        public TownCrier()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} and all is well", DateTime.Now);
+        }
+        public void Start() { _timer.Start(); }
+        public void Stop() { _timer.Stop(); }
+    }
+}
