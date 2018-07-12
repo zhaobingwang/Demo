f8ac6ab98829ac8154922266c47646f8a41a34a2
Route8 <zhaobingwang@foxmail.com>
2018-07-12 16:13:31 +0800

    NLog support

--- Quartz/QuartzWithTopshelf/Jobs/OutputTimeJob.cs
+++ Quartz/QuartzWithTopshelf/Jobs/OutputTimeJob.cs
@@ -1,4 +1,4 @@
-﻿using log4net;
+﻿using NLog;
 using Quartz;
 using System;
 using System.Collections.Generic;
@@ -10,12 +10,25 @@ namespace QuartzWithTopshelf.Jobs
 {
     public class OutputTimeJob : IJob
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
         public Task Execute(IJobExecutionContext context)
         {
+            //var config = new NLog.Config.LoggingConfiguration();
+
+            //var logfile = new NLog.Targets.FileTarget("logfile") { FileName = $"{DateTime.Now.ToString("yyyyMMdd")}.log" };
+            //var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+
+            //config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+            //config.AddRule(LogLevel.Info, LogLevel.Fatal, logfile);
+
+            //LogManager.Configuration = config;
+
             Task t = Task.Factory.StartNew(() =>
             {
-                Console.WriteLine($"CurrentTime:{DateTime.Now}");
+                logger.Info($"CurrentTime:{DateTime.Now}");
+                logger.Warn("hahahaha");
             });
+
             return t;
         }
     }
--- Quartz/QuartzWithTopshelf/QuartzService.cs
+++ Quartz/QuartzWithTopshelf/QuartzService.cs
@@ -1,4 +1,5 @@
-﻿using Quartz;
+﻿using NLog;
+using Quartz;
 using Quartz.Impl;
 using QuartzWithTopshelf.Jobs;
 using System;
@@ -13,8 +14,11 @@ namespace QuartzWithTopshelf
     public sealed class QuartzService
     {
         private readonly IScheduler scheduler;
+        private static Logger logger;
         public QuartzService()
         {
+            logger = LogManager.GetCurrentClassLogger();
+
             // 1.创建作业调度池(Scheduler)
             scheduler = StdSchedulerFactory.GetDefaultScheduler().Result;
             //2.创建一个具体的作业即job (具体的job需要单独在一个文件中执行)
@@ -29,6 +33,7 @@ namespace QuartzWithTopshelf
         public void Start()
         {
             scheduler.Start();
+            logger.Info($"Job {nameof(OutputTimeJob)} start...");
         }
 
         public void Stop()
@@ -36,16 +41,19 @@ namespace QuartzWithTopshelf
             //true：表示该Sheduler关闭之前需要等现在所有正在运行的工作完成才能关闭
             //false：表示直接关闭
             scheduler.Shutdown(false);
+            logger.Info($"Job {nameof(OutputTimeJob)} shutdown...");
         }
 
         public void Continue()
         {
             scheduler.ResumeAll();
+            logger.Info($"Job {nameof(OutputTimeJob)} comtinue...");
         }
 
         public void Pause()
         {
             scheduler.PauseAll();
+            logger.Info($"Job {nameof(OutputTimeJob)} pause...");
         }
     }
 }
