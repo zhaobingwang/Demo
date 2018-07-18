86635987d0908b91ef9860809e072925836f41d8
Route8 <zhaobingwang@foxmail.com>
2018-07-18 17:33:28 +0800

    rabbitmq: Work queues

---
+++ RabbitMQ/NewTask/NewTask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace NewTask
+{
+    class NewTask
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "127.0.0.1" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+                    var message = GetMessage(args);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+                    channel.BasicPublish(exchange: "", routingKey: "task_queue", basicProperties: properties, body: body);
+                    Console.WriteLine($"Sent: {message}");
+                }
+            }
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
---
+++ RabbitMQ/Worker/Worker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading;
+
+namespace Worker
+{
+    class Worker
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "127.0.0.1" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+                Console.WriteLine($" [*] Waiting for message.");
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($" [x] Received {message}");
+
+                    int dots = message.Split('.').Length - 1;
+                    Thread.Sleep(dots * 10000);
+
+                    Console.WriteLine(" [x] Done.");
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                };
+                channel.BasicConsume(queue: "task_queue", autoAck: false, consumer: consumer);
+                Console.WriteLine("Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
