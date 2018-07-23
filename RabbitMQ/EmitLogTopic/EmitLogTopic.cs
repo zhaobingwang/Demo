a13feb77d5c0791c53fdbe8a64d1637e85c65d44
Route8 <zhaobingwang@foxmail.com>
2018-07-23 12:30:26 +0800

    rabbitmq : topics

---
+++ RabbitMQ/EmitLogTopic/EmitLogTopic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Linq;
+
+namespace EmitLogTopic
+{
+    class EmitLogTopic
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "127.0.0.1" };
+            using (var conncetion = factory.CreateConnection())
+            using (var channel = conncetion.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+                var routeKey = (args.Length > 0) ? args[0] : "anonymous.info";
+                var message = (args.Length > 1) ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "topic_logs", routingKey: routeKey, basicProperties: null, body: body);
+                Console.WriteLine($" [x] Sent {routeKey}:{message}");
+            }
+        }
+    }
+}
---
+++ RabbitMQ/ReceiveLogsTopic/ReceiveLogsTopic.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace ReceiveLogsTopic
+{
+    class ReceiveLogsTopic
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "127.0.0.1" };
+            using (var conncetion = factory.CreateConnection())
+            using (var channel = conncetion.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+                var queueName = channel.QueueDeclare().QueueName;
+
+                if (args.Length < 1)
+                {
+                    Console.WriteLine($"Usage:{Environment.GetCommandLineArgs()[0]} [binding_key...]");
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                    Environment.ExitCode = 1;
+                    return;
+                }
+
+                foreach (var bindingKey in args)
+                {
+                    channel.QueueBind(queue: queueName, exchange: "topic_logs", routingKey: bindingKey);
+                }
+
+                Console.WriteLine(" [*] Waiting for message.To exit press CTRL+C");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine($" [x] Received {routingKey}:{message}");
+                };
+                channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
