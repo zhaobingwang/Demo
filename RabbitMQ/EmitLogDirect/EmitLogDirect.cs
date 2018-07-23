5125305c418e7d0009f58bfcbca2568213e4e092
Route8 <zhaobingwang@foxmail.com>
2018-07-23 11:02:28 +0800

    Routing

---
+++ RabbitMQ/EmitLogDirect/EmitLogDirect.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Linq;
+
+namespace EmitLogDirect
+{
+    class EmitLogDirect
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "127.0.0.1" };
+            using (var conncetion = factory.CreateConnection())
+            using (var channel = conncetion.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "direct_logs", type: "direct");
+                var severity = args.Length > 0 ? args[0] : "info";
+                var message = (args.Length > 1) ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World!";
+
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "direct_logs", routingKey: severity, basicProperties: null, body: body);
+                Console.WriteLine($" [x] Sent {severity}:{message}");
+            }
+            Console.WriteLine(" Press [enter] to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ/ReceiveLogsDirect/ReceiveLogsDirect.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace ReceiveLogsDirect
+{
+    class ReceiveLogsDirect
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "127.0.0.1" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "direct_logs", type: "direct");
+                var queueName = channel.QueueDeclare().QueueName;
+
+                if (args.Length < 1)
+                {
+                    Console.WriteLine($"Usage:{Environment.GetCommandLineArgs()[0]} [info] [warning] [error]");
+                    Console.WriteLine(" Press [entet] to exit.");
+                    Console.ReadLine();
+                    Environment.ExitCode = 1;
+                    return;
+                }
+
+                foreach (var severity in args)
+                {
+                    channel.QueueBind(queue: queueName, exchange: "direct_logs", routingKey: severity);
+                }
+                Console.WriteLine(" [*] Waiting for message.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine($" [x] Received {routingKey}:{message}");
+                };
+                channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+                Console.ReadLine();
+            }
+        }
+    }
+}
