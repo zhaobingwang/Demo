c187b851d5d28ebf1a3a5c4070b9731c69164fb2
Route8 <zhaobingwang@foxmail.com>
2018-07-26 21:20:45 +0800

    实体转json & xml转json

---
+++ NewtonsoftJsonExample/JsonDemo/JsonDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace JsonDemo
+{
+    class JsonDemo
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Name = "Watermelon";
+            product.ExpireDate = new DateTime(2019, 8, 31);
+            product.Price = 29.99m;
+            product.Sizes = new string[] { "Small", "Medium", "Large" };
+            string output = JsonConvert.SerializeObject(product);
+            Console.WriteLine(output);
+        }
+    }
+
+    class Product
+    {
+        public string Name { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public decimal Price { get; set; }
+        public string[] Sizes { get; set; }
+    }
+}
---
+++ NewtonsoftJsonExample/XML2JSON/XML2JSON.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Xml;
+
+namespace XML2JSON
+{
+    class XML2JSON
+    {
+        static void Main(string[] args)
+        {
+            string xml = @"<?xml version='1.0' standalone='no'?>
+                            <root>
+                              <person id='1'>
+                              <name>Alan</name>
+                              <url>http://www.google.com</url>
+                              </person>
+                              <person id='2'>
+                              <name>Louis</name>
+                              <url>http://www.yahoo.com</url>
+                              </person>
+                            </root>";
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+
+            string json = JsonConvert.SerializeXmlNode(doc);
+            Console.WriteLine(json);
+        }
+    }
+}
