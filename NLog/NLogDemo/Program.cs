bf01a6432fd5e9bbb28af7e4a2cab97a83566991
Route8 <zhaobingwang@foxmail.com>
2018-08-07 08:21:09 +0800

    nlog demo

---
+++ NLog/NLogDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NLog;
+
+namespace NLogDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = LogManager.GetCurrentClassLogger();
+            logger.Trace("Trace Message");
+            logger.Debug("Debug Message");
+            logger.Info("Info Message");
+            logger.Error("Error Message");
+            logger.Fatal("Fatal Message");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ NLog/WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly Logger logger = LogManager.GetCurrentClassLogger();
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            logger.Trace("Trace Message");
+            logger.Debug("Debug Message");
+            logger.Info("Info Message");
+            logger.Error("Error Message");
+            logger.Fatal("Fatal Message");
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
