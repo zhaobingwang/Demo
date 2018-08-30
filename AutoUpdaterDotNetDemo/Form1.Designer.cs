3ec8fb2bc8a5d378cf5050ae657035767f0a87ba
Route8 <zhaobingwang@foxmail.com>
2018-08-30 14:38:29 +0800

    winform auto update

--- AutoUpdaterDotNetDemo/Form1.Designer.cs
+++ AutoUpdaterDotNetDemo/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.version = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(53, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // version
+            // 
+            this.version.AutoSize = true;
+            this.version.Location = new System.Drawing.Point(277, 387);
+            this.version.Name = "version";
+            this.version.Size = new System.Drawing.Size(47, 12);
+            this.version.TabIndex = 1;
+            this.version.Text = "version";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(282, 273);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.version);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label version;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- AutoUpdaterDotNetDemo/Form1.cs
+++ AutoUpdaterDotNetDemo/Form1.cs
@@ -2,18 +2,31 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using AutoUpdaterDotNET;
 
 namespace AutoUpdaterDotNetDemo
 {
     public partial class Form1 : Form
     {
+        string updateUrl = "http://172.18.34.182:8001/updates/UpdateConfig.xml";
         public Form1()
         {
             InitializeComponent();
+            AutoUpdater.Start(updateUrl);
+
+            
+            version.Text = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AutoUpdater.Start(updateUrl);
         }
     }
 }
